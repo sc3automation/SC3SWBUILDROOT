b1609f20368495bf159f882a5524e71596bc7908
Frederic B. Desy <fbdesy@sc3automation.com>
2025-03-27 12:04:35 +1100

    Add build script for MCB11r6 Fix configuration file MCB11r6

---
+++ build_mcb11r6.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#Store version in rootfs
+echo $1.$2.$3-$4 > board/sc3auto/mcb11/rootfs_overlay/etc/sc3version 
+
+make sc3auto_mcb11r6_defconfig
+make
+rm output/target/etc/ld.so.conf
+make
+
+cp ./output/images/MLO .
+cp ./output/images/u-boot.img .
+cp ./output/images/zImage .
+cp ./output/images/am437x-mcb11.dtb .
+cp ./output/images/rootfs.tar .
+
+zip SW-1084-E$1.$2.$3-$4.zip ./zImage ./am437x-mcb11.dtb ./MLO ./u-boot.img ./rootfs.tar
