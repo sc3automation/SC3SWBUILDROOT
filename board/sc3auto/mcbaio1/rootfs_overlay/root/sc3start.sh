84f097d73abfcf78a7fea767095e23a612d5a559
Frederic B. Desy <fbdesy@sc3automation.com>
2025-06-19 08:51:30 +1000

    Reduce size of CAN transmission queue

--- board/sc3auto/mcbaio1/rootfs_overlay/root/sc3start.sh
+++ board/sc3auto/mcbaio1/rootfs_overlay/root/sc3start.sh
@@ -103,7 +103,7 @@ echo out > /sys/class/gpio/gpio155/direction
 # need to setup baudrate of CAN port
 #ip link set can0 down
 #ip link set can0 type can bitrate 500000
-#ip link set can0 txqueuelen 1000
+#ip link set can0 txqueuelen 10
 #ip link set can0 up
 
 chmod a+x /root/nodeRtLnx
